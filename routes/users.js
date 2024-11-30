6557837d171bc52d957329aab7719b8759932822
SinglaAnsh <anshsingla961@gmail.com>
2024-11-29 19:27:14 -0500

    Fix OAuth2Strategy clientID and clientSecret configuration

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
-var express = require('express');
-var router = express.Router();
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const passportLocalMongoose = require('passport-local-mongoose');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+  // Add any additional user fields you need
 });
 
-module.exports = router;
+userSchema.plugin(passportLocalMongoose);
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
+
